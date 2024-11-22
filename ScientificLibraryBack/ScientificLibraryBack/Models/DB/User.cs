e02e7181a394c29a186bfe41401e5ded198eea6a
Zaruhi Hatsagortsyan <Zaruhi Hatsagortsyan@armbusinessbank.local>
2024-11-22 17:18:39 +0400

    first commit

---
+++ ScientificLibraryBack/ScientificLibraryBack/Models/DB/User.cs
@@ -0,0 +1,14 @@
+﻿namespace ScientificLibraryBack.Models.DB
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
